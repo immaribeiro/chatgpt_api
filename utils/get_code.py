62ab331512dc317cd0ba51e74184ec736689b618
immaribeiro <imma.ribeiro@gmail.com>
2023-02-11 16:50:51 +0000

    add cmd line feature

--- utils/get_code.py
+++ utils/get_code.py
@@ -1,21 +1,27 @@
 import openai
+import os
+import json
 
-prompt_code = input('\nWhat code you want to generate?  \n')
-#prompt_code = 'How do I read a CSV file in Python?'
+#prompt_code = input('\nWhat code you want to generate?  \n')
 
 def get_code(prompt_code):
+    os.makedirs('code_output/', exist_ok=True)
     completion = openai.Completion.create(
             engine='davinci-codex',
             prompt=prompt_code,
             max_tokens=200,
-            n=5,
+            n=1,
             stop=None,
             temperature=0.1,
             frequency_penalty=0.9,
             presence_penalty=0.6,
             #model="text-davinci-002"
         )
+    
+    #selecting only the first result to print
+    code = completion.choices[0].text
+    #writing to file all the results
+    with open('code_output/' + prompt_code + '.json' , 'wb') as f:
+            f.write(json.dumps(completion).encode())
 
-    print(completion.choices[0].text)
-   # return completion
-get_code(prompt_code)
+    return code
--- utils/get_images.py
+++ utils/get_images.py
@@ -3,9 +3,9 @@ import json
 import os
 import openai
 
-prompt_image = input('\nWhat image do you want? \n')
+#prompt_image = input('\nWhat image do you want? \n')
 
-def download_images(response):
+def download_images(response, prompt_image):
     os.makedirs('images_output/', exist_ok=True)
     images_output_path = 'images_output/'
     image_counter = 0    
@@ -14,22 +14,23 @@ def download_images(response):
         response = requests.get(url)
         image_counter += 1
 
-        with open(images_output_path + 'image_' + prompt_image.replace(" ", "_") + '_' + str(image_counter) + '.png' , 'wb') as f:
+        with open(images_output_path + prompt_image.replace(" ", "_") + '_' + str(image_counter) + '.png' , 'wb') as f:
             f.write(response.content)
+    
+    return f
 
 
 def get_images(prompt_image):
     image_size='256x256'
     image_number=1
     response = openai.Image.create(prompt=prompt_image, n=image_number, size=image_size)
+    download_images(response, prompt_image)
     os.makedirs('json_responses', exist_ok=True)
     response_json = json.dumps(response, indent=4)
 
     with open('json_responses/' + 'images_response_' + prompt_image + '.json' , 'wb') as f:
             f.write(response_json.encode())
-    download_images(response)
 
-get_images(prompt_image)
-
+    return response
 
 #def create_image_variatoins(json_response): WIP
